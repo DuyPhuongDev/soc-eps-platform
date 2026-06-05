52b357207759df41edd9b67c4dfd3cf93cc9a266
dphuongdev <duyphuong.devg@gmail.com>
2026-06-05 16:25:19 +0700

    initial project

---
+++ license/src/test/java/com/vdt/soc/license/LicenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdt.soc.license;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
